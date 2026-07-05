f773a5cf88b3f8be49f707c4bfea4cdade500696
loremarcomin <almightyfern0@gmail.com>
2026-07-06 00:26:48 +0200

    footer first version

---
+++ src/lib/slices/Footer/index.svelte
@@ -0,0 +1,92 @@
+<script lang="ts">
+	import type { Content } from '@prismicio/client';
+	import type { SliceComponentProps } from '@prismicio/svelte';
+	import { PrismicRichText } from '@prismicio/svelte';
+	import tnLogo from '$lib/assets/tnLogo.png';
+	import {page} from '$app/state';
+
+	type Props = SliceComponentProps<Content.FooterSlice>;
+
+	const { slice }: Props = $props();
+</script>
+
+<footer class="grid grid-cols-8 gap-x-4 w-full my-40">
+    <h1 class="col-span-8 text-center">{slice.primary.motto}</h1>
+
+	{#if slice.variation == 'footerPerformer'}
+		<div class="col-span-8 flex flex-row w-full my-40">
+			<a href="http://google.com" class="uppercase">
+				<b>Perfomer</b>
+			</a>
+
+			<hr class="flex-1 border-white mx-4 mt-3.5">
+
+			<p class="text-center capitalize"><b>Altri Performer</b></p>
+
+			<hr class="flex-1 border-white mx-4 mt-3.5">
+
+			<a href="http://google.com" class="text-right uppercase">
+				<b>Performer</b>
+			</a>
+		</div>
+
+	{:else if slice.variation == 'default'}
+
+		<div class="col-span-8 flex flex-row w-full my-40">
+			<a href="http://google.com" class="uppercase">
+				<b>show</b>
+			</a>
+
+			<hr class="flex-1 border-white mx-4 mt-3.5">
+
+			<p class="text-center capitalize"><b>Altri Show</b></p>
+
+			<hr class="flex-1 border-white mx-4 mt-3.5">
+
+			<a href="http://google.com" class="text-right uppercase">
+				<b>show</b>
+			</a>
+		</div>
+	{/if}
+
+    <div class="col-span-8 grid grid-cols-subgrid">
+        <div class="col-span-2">
+            <ul>
+                <li><a href="https://google.com" class="text-sm">contact@thenerve.com</a></li>
+                <li><a href="https://google.com" class="text-sm">talents@thenerve.com</a></li>
+                <li><a href="https://google.com" class="text-sm">+39 123 4567 890</a></li>
+                <br>
+                <li><a href="https://google.com" class="text-sm uppercase">instagram</a></li>
+            </ul>
+        </div>
+
+        <div class="col-span-2">
+            <ul>
+                <li>
+                    <a href="https://google.com" class="text-sm">
+                        <b class="uppercase font-bold">Offtopic</b><br>
+                        <span>Lungo Dora Firenze, 131/A, 10153 Torino TO</span>
+                    </a>
+                </li>
+                <br>
+                <li>
+                    <a href="https://google.com" class="text-sm">
+                        <b class="uppercase font-bold">Q35</b><br>
+                        <span>Lungo Dora Firenze, 131/A, 10153 Torino TO</span>
+                    </a>
+                </li>
+            </ul>
+        </div>
+
+        <div class="col-span-3 col-start-6">
+            <a href="/" aria-current={page.url.pathname === '/'}><img class="w-full -mt-8" src={tnLogo} alt="Logo"></a>
+        </div>
+
+        <div class="col-span-4 mt-8">
+            <span class="text-sm text-white">Design and code by <b class="smallFooter"><a href="https://lorenzomarcomin.com" target="_blank" rel="noopener noreferrer">Lorenzo Marcomin</a></b></span>
+        </div>
+        <div class="col-span-4 mt-8 text-right">
+            <span class="text-sm text-white">© All rights reserved. 2026 The Nerve</span>
+        </div>
+    </div>
+</footer>
---
+++ src/lib/slices/Grid/index.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	import type { Content } from '@prismicio/client';
+	import type { SliceComponentProps } from '@prismicio/svelte';
+
+	type Props = SliceComponentProps<Content.GridSlice>;
+
+	const { slice }: Props = $props();
+</script>
+
+<section
+	class="relative top-0 col-span-8 grid grid-cols-1 md:grid-cols-4 lg:grid-cols-8 auto-rows-0.25 gap-x-4 gap-y-0"
+	data-slice-type={slice.slice_type}
+	data-slice-variation={slice.variation}
+>
+	{#each slice.primary.grid_element as { grid_img }, i}
+		{@const isTallColumn = i === 1 || i === 3}
+		<div
+			class="col-span-1 md:col-span-2 row-span-316 md:row-span-406 {isTallColumn
+				? 'lg:row-span-591'
+				: 'lg:row-span-476'}"
+		>
+			<img
+				src={grid_img.url}
+				alt={grid_img.alt ?? ''}
+				class="w-full object-cover h-[300px] md:h-[390px] {isTallColumn
+					? 'lg:h-[575px]'
+					: 'lg:h-[460px]'}"
+			>
+		</div>
+	{/each}
+</section>
