0e42b634f87b4f8c1e77469e47f3a0ffe01a9590
loremarcomin <almightyfern0@gmail.com>
2026-07-05 16:13:45 +0200

    prismic preview fixes

--- src/lib/slices/PerformerHero/index.svelte
+++ src/lib/slices/PerformerHero/index.svelte
@@ -28,7 +28,7 @@
     <div class="grid col-span-8 grid-cols-subgrid">
         <h1 class="sticky top-30 -mb-2.5 col-span-5 z-10">{slice.primary.nome_performer}</h1>
         <span class="col-span-4 col-start-5 z-50 mt-40">
-            <div class="col-span-4 col-start-5">
+            <div class="col-span-4 col-start-5 max-h-200">
                 <PrismicRichText field={slice.primary.bio} />
             </div>
             <PrismicLink field={slice.primary.instagram} class="col-start-5 mt-8 flex gap-2 max-w-fit">
